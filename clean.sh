5f6b33dcbe21fcba248c3bd19ff231086a976ca5
jerome <alet@librelogiciel.com>
2010-04-20 22:34:58 +0000

    Added the PyKoCard subproject, now that I've finally put my hands onto a real manual !!! Cartadis TCRS vending card readers / encoders will be supported soon, they are used in most libraries of French universities, at least. More than that : the API is small and code will be easy to write :-)
    I think it's better to put all this outside of PyKota since it will
    probably be used mostly with Tea4CUPS alone, or a combination of
    Tea4CUPS and PyKota. This Python module will be useable without any of
    these software too.

---
+++ clean.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+#
+# PyKoCard
+#
+# PyKoCard : Smart Card / Vending Card managing library
+#
+# (c) 2010 Jerome Alet <alet@librelogiciel.com>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# $Id$
+
+# Use this to clean the tree from temporary files
+
+rm -f MANIFEST ChangeLog
+find . -name "*.bak" -exec rm -f {} \;
+find . -name "\#*\#" -exec rm -f {} \;
+find . -name "*~" -exec rm -f {} \;
+find . -name "*.pyc" -exec rm -f {} \;
+find . -name "*.pyo" -exec rm -f {} \;
+find docs -name "*.html" -exec rm -f {} \;
+find docs -name "*.pdf" -exec rm -f {} \;
+find docs -name "*.tex" -exec rm -f {} \;
+find docs -name "*.dvi" -exec rm -f {} \;
+rm -fr build dist
+
